d097107f6e6732d4a3b0819359e36309afc83ebf
Ragiri Rajasekhar <ragirirajasekhar@gmail.com>
2018-11-26 09:48:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mazhar/demofoupushinggithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mazhar.demofoupushinggithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
